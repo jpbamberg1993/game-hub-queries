9001c4864375084cbc6cd48f771e6b0981230dd7
Paul Bamberg <jpbamberg1993@gmail.com>
2023-12-28 17:04:29 -0500

    Add query for getting records by name

--- query.js
+++ query.js
@@ -9,13 +9,14 @@ let connectionString = env === 'prod'
 connectionString += "?sslmode=require"
 
 const pool = new Pool({
-    connectionString
+	connectionString
 })
 
 const client = await pool.connect()
 
 try {
-	const data = await getGamesWithChildren()
+	const { rows } = await getGameByName()
+	console.table(rows)
 } catch (error) {
 	await client.query('ROLLBACK')
 	console.error(`Transaction error: ${error.stack}`)
@@ -24,13 +25,12 @@ try {
 }
 
 async function getGames() {
-	const { rows } = await client.query(`
+	return await client.query(`
 		select g.*
 		from games g
 		join games_to_genres gtg on g.id = gtg.game_id
 		where gtg.genre_id = 4
 	`);
-	console.table(rows)
 }
 
 async function deleteRecords() {
@@ -44,14 +44,26 @@ async function deleteRecords() {
 async function getGamesWithChildren() {
 	return await client.query(`
 		SELECT g.id, g.name, g.background_image, json_agg(p) as platforms
-		FROM games g
+		FROM (
+			SELECT g.*
+			FROM games g
+			JOIN games_to_platforms gtp on gtp.game_id = g.id
+			JOIN platforms p on gtp.platform_id = p.id
+			WHERE p.slug = 'pc'
+		) as g
 		JOIN games_to_platforms gtp on gtp.game_id = g.id
 		JOIN platforms p on gtp.platform_id = p.id
 		JOIN games_to_genres gtg on gtg.game_id = g.id
 		JOIN genres genres on genres.id = gtg.genre_id
 		WHERE genres.slug = 'action'
-		GROUP BY g.id
+		GROUP BY g.id, g.name, g.background_image
 		LIMIT 20
+	`)
+}
+
+async function getGameByName() {
+	return await client.query(`
+		SELECT * FROM games WHERE name ILIKE LOWER('Little Misfortune')
 	`)
 }
 
